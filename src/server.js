930694b0c2f71a0f2ec1776c5f5ca591d00cdc0a
FaizanHashmi3 <mdfaizanhashmi3@gmail.com>
2024-01-02 21:08:58 +0530

    link changed

--- src/server.js
+++ src/server.js
@@ -3,16 +3,23 @@ const express = require('express');
 const cors = require('cors'); 
 const axios = require('axios');
 const https = require('https');
+const path = require('path');
 const app = express();
+
 app.use(express.urlencoded({ extended: true }))
 app.use(cors());
 app.use(express.json());
+const __dirname1 = path.resolve();
+app.use(express.static(path.join(__dirname1, "/src/build")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname1, "src", "build", "index.html"));
+});
 var token = '';
 var url = " ";
 
 const clientId = '3MVG95mg0lk4batjVwHpPkahCROA1JXckG2MoWXqDTqMcv2sI4NjLmzIJq33BJka_FCR0TwINW3LN.Yuclvxa';
 const clientSecret = '4296A62A96D50C0A3B928A183176446BF37CE71B889293DE61607C9BD34E2AE0';
-const redirectUri = 'http://localhost:3000'; 
+const redirectUri = 'https://salesforce-switch-1u2a.onrender.com'; 
 
 const salesforceAPI = 'https://cheems6-dev-ed.develop.my.salesforce.com/services/data/v33.0/tooling/sobjects/ValidationRule/';
 
